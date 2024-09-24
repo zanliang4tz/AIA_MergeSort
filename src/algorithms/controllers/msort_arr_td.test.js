655b79581ffad971033149734fc2821d98745d9e
ZAN LIANG <zaliang@student.unimelb.edu.au>
2024-09-24 13:01:44 +1000

    created automated tetsing file for top down  merge sort

---
+++ src/algorithms/controllers/msort_arr_td.test.js
@@ -0,0 +1,44 @@
+
+/* The purpose of the test here is to detect whether the correct result is generated
+   under the legal input, not to test its robustness, because this is not considered
+   in the implementation process of the algorithm.
+*/
+
+/* eslint-disable no-undef */
+
+import msort_arr_td from './msort_arr_td';
+
+// Simple stub for the chunker
+const chunker = {
+  add: () => { },
+};
+
+describe('msort_arr_td', () => {
+  it('sorts empty array', () => {
+    expect(msort_arr_td.run(chunker, { nodes: [] })).toEqual([]);
+  });
+  it('sorts single element', () => {
+    expect(msort_arr_td.run(chunker, { nodes: [4] })).toEqual([4]);
+  });
+  it('sorts two values', () => {
+    expect(msort_arr_td.run(chunker, { nodes: [1, 2] })).toEqual([1, 2]);
+  });
+  it('sorts two values in reverse', () => {
+    expect(msort_arr_td.run(chunker, { nodes: [2, 1] })).toEqual([1, 2]);
+  });
+  it('sorts odd list', () => {
+    expect(msort_arr_td.run(chunker, { nodes: [1, 4, 3] })).toEqual([1, 3, 4]);
+  });
+  it('sorts even list', () => {
+    expect(msort_arr_td.run(chunker, { nodes: [1, 4, 3, 2] })).toEqual([1, 2, 3, 4]);
+  });
+  it('sorts ordered elements', () => {
+    expect(msort_arr_td.run(chunker, { nodes: [1, 2, 3, 4, 5] })).toEqual([1, 2, 3, 4, 5]);
+  });
+  it('sorts reversed elements', () => {
+    expect(msort_arr_td.run(chunker, { nodes: [5, 4, 3, 2, 1] })).toEqual([1, 2, 3, 4, 5]);
+  });
+  it('sorts list with duplicates', () => {
+    expect(msort_arr_td.run(chunker, { nodes: [1, 3, 2, 5, 3, 2] })).toEqual([1, 2, 2, 3, 3, 5]);
+  });
+});
